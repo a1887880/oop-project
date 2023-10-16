1bdfae10694860617573a53bb9d94431367d081c
kkiveric <140364069+kkiveric@users.noreply.github.com>
2023-10-16 21:40:55 +1030

    Add files via upload

---
+++ PotatoTesting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Potato.h"
+
+int main() {
+    // Create a Potato object
+    Potato potato;
+
+    // Test summarizing the commodity
+    potato.summariseCommodity();
+
+    return 0;
+}
---
+++ WheatTesting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Wheat.h"
+
+int main() {
+    // Creating a Wheat object
+    Wheat wheat;
+
+    // Testing summarizing the commodity
+    wheat.summariseCommodity();
+
+    return 0;
+}
